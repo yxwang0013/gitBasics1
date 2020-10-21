475c06fd3dce0a3d8bfabdb4f947a30968274255
yuxuan wang <2879296442@qq.com>
2020-10-21 16:46:44 +0100

    Implemented and tested removeProductById

--- src/productCatalogue.js
+++ src/productCatalogue.js
@@ -16,5 +16,14 @@ class Catalogue {
     }
     return false;
   }
+  removeProductById(id) {
+    const removedProduct = this.findProductById(id);
+    if (removedProduct) {
+      this.products = this.products.filter(
+        (product) => product.id !== id // ***********
+      );
+    }
+    return removedProduct;
+  }
 }
 module.exports = Catalogue;
--- test/catalogue-test.js
+++ test/catalogue-test.js
@@ -13,6 +13,12 @@ cat.addProduct(new Product("A124", "Product 2", 100, 10.0));
 cat.addProduct(new Product("A125", "Product 3", 100, 10, 10.0));
 
 describe("Catalogue", () => {
+    beforeEach(() => {
+        cat = new Catalogue("Test Catalogue");
+        cat.addProduct(new Product("A123", "Product 1", 100, 10, 10.0, 10));
+        cat.addProduct(new Product("A124", "Product 2", 100, 10.0, 10));
+        cat.addProduct(new Product("A125", "Product 3", 100, 10, 10.0, 10));
+      });
   describe("findProductById", function () {
     it("should find a valid product id", function () {
       const result = cat.findProductById("A123");
@@ -23,4 +29,18 @@ describe("Catalogue", () => {
       expect(result).to.be.undefined;
     });
   });
+  describe("removeProductById", () => {
+    it("should remove product with a valid id", function () {
+      let result = cat.removeProductById("A123");
+      expect(result.id).to.equal("A123");
+      // Check object state
+      result = cat.findProductById("A123");
+      expect(result).to.be.undefined;
+    });
+    it("should return undefined when asked to remove invalid product", function () {
+      const result = cat.removeProductById("A321");
+      expect(result).to.be.undefined;
+    });
+  });
 });
+
