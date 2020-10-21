b4e209d542c6bcbe339c35fb9897bb8c91f07beb
yuxuan wang <2879296442@qq.com>
2020-10-21 16:21:17 +0100

    Tested findProductById

--- test/catalogue-test.js
+++ test/catalogue-test.js
@@ -2,3 +2,25 @@ const chai = require("chai");
 const Catalogue = require("../src/productCatalogue");
 const Product = require("../src/product");
 const expect = chai.expect;
+
+let cat = null;
+let batch = null;
+
+// Setup
+cat = new Catalogue("Test Catalogue");
+cat.addProduct(new Product("A123", "Product 1", 100, 10, 10.0));
+cat.addProduct(new Product("A124", "Product 2", 100, 10.0));
+cat.addProduct(new Product("A125", "Product 3", 100, 10, 10.0));
+
+describe("Catalogue", () => {
+  describe("findProductById", function () {
+    it("should find a valid product id", function () {
+      const result = cat.findProductById("A123");
+      expect(result.name).to.equal("Product 1");
+    });
+    it("should return undefined for invalid product id", function () {
+      const result = cat.findProductById("A321");
+      expect(result).to.be.undefined;
+    });
+  });
+});
